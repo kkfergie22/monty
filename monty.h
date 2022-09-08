f049d9db76f58983b5c545dc2f0764c85465c746
Christian Koranteng <korantengchristian@gmail.com>
2022-09-08 15:32:17 +0000

    Update repo

--- monty.h
+++ monty.h
@@ -18,15 +18,14 @@ extern unsigned int line_number;
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO project
  */
 typedef struct stack_s
 {
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
+  int n;
+  struct stack_s *prev;
+  struct stack_s *next;
 } stack_t;
-
 
 /**
  * struct instruction_s - opcode and its function
@@ -38,8 +37,8 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
+  char *opcode;
+  void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 stack_t *build_list(stack_t *head);
