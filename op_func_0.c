6ece0853d3685d932cb3589798b93eafae83045a
Christian Koranteng <korantengchristian@gmail.com>
2022-09-08 15:48:51 +0000

    Update repo

--- op_func_0.c
+++ op_func_0.c
@@ -7,27 +7,27 @@
  */
 void _push(stack_t **stack, unsigned int n)
 {
-	stack_t *node = NULL;
-	(void) n;
+  stack_t *node = NULL;
+  (void)n;
 
-	node = malloc(sizeof(stack_t));
-	if (!node)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		free_stack(stack);
-		exit(EXIT_FAILURE);
-	}
+  node = malloc(sizeof(stack_t));
+  if (!node)
+  {
+    fprintf(stderr, "Error: malloc failed\n");
+    free_stack(stack);
+    exit(EXIT_FAILURE);
+  }
 
-	node->prev = node->next = NULL;
+  node->prev = node->next = NULL;
 
-	if (!(*stack))
-		(*stack) = node;
-	else
-	{
-		(*stack)->prev = node;
-		node->next = *stack;
-		*stack = node;
-	}
+  if (!(*stack))
+    (*stack) = node;
+  else
+  {
+    (*stack)->prev = node;
+    node->next = *stack;
+    *stack = node;
+  }
 }
 
 /**
@@ -37,18 +37,18 @@ void _push(stack_t **stack, unsigned int n)
  */
 void _pall(stack_t **stack, unsigned int n)
 {
-	stack_t *node, *head;
-	(void) n;
+  stack_t *node, *head;
+  (void)n;
 
-	if (!stack || !(*stack))
-		return;
-	node = head = *stack;
+  if (!stack || !(*stack))
+    return;
+  node = head = *stack;
 
-	while (node)
-	{
-		fprintf(stdout, "%d\n", node->n);
-		node = node->next;
-	}
+  while (node)
+  {
+    fprintf(stdout, "%d\n", node->n);
+    node = node->next;
+  }
 }
 
 /**
@@ -58,39 +58,39 @@ void _pall(stack_t **stack, unsigned int n)
  */
 void _swap(stack_t **stack, unsigned int line_number)
 {
-	int store = 0;
+  int store = 0;
 
-	if ((stack == NULL) || (*stack == NULL) || ((*stack)->next) == NULL)
-	{
-		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
-		if (*stack)
-			free_stack(stack);
-		exit(EXIT_FAILURE);
-	}
+  if ((stack == NULL) || (*stack == NULL) || ((*stack)->next) == NULL)
+  {
+    fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+    if (*stack)
+      free_stack(stack);
+    exit(EXIT_FAILURE);
+  }
 
-	(void) line_number;
+  (void)line_number;
 
-	store = (*stack)->next->n;
-	(*stack)->next->n = (*stack)->n;
-	(*stack)->n = store;
+  store = (*stack)->next->n;
+  (*stack)->next->n = (*stack)->n;
+  (*stack)->n = store;
 }
 
 /**
- * _add - swaps the data in two nodes
+ * _add - adds the data in two nodes
  * @stack: pointer to a stack_t
  * @line_number: line number
  */
 void _add(stack_t **stack, unsigned int line_number)
 {
-	if (stack == NULL || (*stack == NULL) || ((*stack)->next == NULL))
-	{
-		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
-		if (*stack)
-			free_stack(stack);
-		exit(EXIT_FAILURE);
-	}
-	(*stack)->next->n = ((*stack)->next->n) + ((*stack)->n);
-	_pop(stack, line_number);
+  if (stack == NULL || (*stack == NULL) || ((*stack)->next == NULL))
+  {
+    fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+    if (*stack)
+      free_stack(stack);
+    exit(EXIT_FAILURE);
+  }
+  (*stack)->next->n = ((*stack)->next->n) + ((*stack)->n);
+  _pop(stack, line_number);
 }
 
 /**
@@ -100,13 +100,13 @@ void _add(stack_t **stack, unsigned int line_number)
  */
 void _sub(stack_t **stack, unsigned int line_number)
 {
-	if (stack == NULL || (*stack == NULL) || ((*stack)->next == NULL))
-	{
-		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
-		if (*stack)
-			free_stack(stack);
-		exit(EXIT_FAILURE);
-	}
-	(*stack)->next->n = ((*stack)->next->n) - ((*stack)->n);
-	_pop(stack, line_number);
+  if (stack == NULL || (*stack == NULL) || ((*stack)->next == NULL))
+  {
+    fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+    if (*stack)
+      free_stack(stack);
+    exit(EXIT_FAILURE);
+  }
+  (*stack)->next->n = ((*stack)->next->n) - ((*stack)->n);
+  _pop(stack, line_number);
 }
