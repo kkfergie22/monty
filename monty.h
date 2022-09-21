388a65099fe5702301cb3c325e35e20cf0ae7fb0
Christian Koranteng <korantengchristian@gmail.com>
2022-09-21 11:13:00 +0000

    Update monty.h

--- monty.h
+++ monty.h
@@ -33,7 +33,7 @@ typedef struct stack_s
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO project
  */
 typedef struct instruction_s
 {
